4d4c5a1fd8e4f1d70782ac0b05ad756b2966fe78
wonderkid-web <mwahyuap2018@gmail.com>
2024-06-29 09:29:13 +0700

    adding icon on sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,8 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import logo from "@/../public/favicon.ico"
+import Image from 'next/image';
 
 const Sidebar = () => {
   const pathname = usePathname()
@@ -10,6 +12,9 @@ const Sidebar = () => {
 
   return (
     <div className="h-screen bg-gray-800 text-white p-4">
+       <div className='relative w-8 h-8 mx-auto mb-2'>
+        <Image src={logo} alt='logo' objectFit='cover' />
+      </div>
       <div className="text-center text-xl font-bold mb-8">E-Presensi</div>
       <nav className="space-y-4">
         <Link href="/" className={`block px-4 py-2 rounded hover:bg-gray-700 ${pathname === '/dashboard' && 'bg-gray-700'}`}>
